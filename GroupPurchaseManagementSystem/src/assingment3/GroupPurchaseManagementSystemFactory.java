881efa32f9e082654fad2a2000751bcc4a9a4893
Grace <hmybbok@126.com>
2017-06-23 00:27:05 +0800

    rmi

--- GroupPurchaseManagementSystem/src/assingment3/GroupPurchaseManagementSystemFactory.java
+++ GroupPurchaseManagementSystem/src/assingment3/GroupPurchaseManagementSystemFactory.java
@@ -1,12 +1,14 @@
 /** <a href="http://www.cpupk.com/decompiler">Eclipse Class Decompiler</a> plugin, Copyright (c) 2017 Chen Chao. **/
-package assingment3;
+package assignment3;
+
+import java.rmi.RemoteException;
 
 import assignment3.ShortMessageSender;
-
+import assingment3.BankSystem;
 
 public class GroupPurchaseManagementSystemFactory {
 	public static GroupPurchaseManagementSystem createGroupPurchaseManagementSystem(
-			ShortMessageSender sms, BankSystem bank) {
+			ShortMessageSender sms, BankSystem bank) throws RemoteException {
 		return new GroupPurchaseManagementSystemImpl(sms, bank);
 	}
 }
