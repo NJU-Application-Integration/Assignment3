bfa75f980297a0f534d73c920bcaa5394238a959
ShirokoSama <141250108@smail.nju.edu.cn>
2017-06-24 12:59:33 +0800

    corba start

---
+++ GroupPurchaseManagementSystem/src/main/java/com/nju/groupPurchaseManagementSystem/ratailCorba/CorbaServer.java
@@ -0,0 +1,13 @@
+package com.nju.groupPurchaseManagementSystem.ratailCorba;
+
+import org.omg.CORBA.ORB;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Srf on 2017/6/24
+ */
+
+@Component
+public class CorbaServer {
+
+}
