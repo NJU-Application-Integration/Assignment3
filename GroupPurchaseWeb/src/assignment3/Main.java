22d10201057b03aef68fcf33556ef65ab76e0336
hanmy <hmybbok@126.com>
2017-06-24 14:34:57 +0800

    update about web

--- GroupPurchaseWeb/src/assignment3/Main.java
+++ GroupPurchaseWeb/src/assignment3/Main.java
@@ -7,21 +7,7 @@ import assignment3.GroupPurchaseManagementSystemImpl;
 public class Main {
 
 	public static void main(String [] args){
-		ShortMessageSender sms = ShortMessageSenderFactory.createShortMessageSender();
-		
-		
-		GroupPurchaseManagementSystem gpms;
-		
-		try {
-			gpms = new GroupPurchaseManagementSystemImpl(sms,null);
-			RmiStarter rmiStarter=new RmiStarter(gpms);
 			RmiClient rmiClient=new RmiClient();
 			GroupPurchaseWeb.launch(rmiClient.getGroupPurchaseManagementSystem());
-		
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 	}
 }
